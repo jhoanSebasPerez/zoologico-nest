88ae080a64125357ba99defbd8b3e1ed5839ca54
Jhoan Pérez <jhoansebastianperez73@gmail.com>
2025-02-11 17:04:08 -0500

    remove regex pattern for password validation when user login

--- src/auth/dto/request/login-user.dto.ts
+++ src/auth/dto/request/login-user.dto.ts
@@ -9,9 +9,5 @@ export class LoginUserDto {
     @IsString()
     @MinLength(6)
     @MaxLength(50)
-    @Matches(
-        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d|.*\W).*$/, {
-        message: 'The password must have a Uppercase, lowercase letter and a number'
-    })
     password: string;
 }
